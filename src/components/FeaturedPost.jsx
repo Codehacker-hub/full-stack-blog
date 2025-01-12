4db33fbc0aeca260744aa8923a39743cd95a0d9c
Ashutosh <att567893393@gmail.com>
2025-01-12 18:33:13 +0530

    categories

---
+++ src/components/FeaturedPost.jsx
@@ -0,0 +1,7 @@
+const FeaturedPost = () => {
+  return (
+    <div>FeaturedPost</div>
+  );
+};
+
+export default FeaturedPost;
---
+++ src/components/MainCategories.jsx
@@ -0,0 +1,94 @@
+import { Link } from "react-router-dom";
+const MainCategories = () => {
+  return (
+    <div className="hidden md:flex bg-white rounded-3xl xl:rounded-full p-4 shadow-lg items-center justify-center gap-8 ">
+      {/* Links */}
+      <div className="flex-1 flex items-center flex-wrap gap-4">
+        <Link
+          to="/posts"
+          className="bg-blue-800 text-white rounded-full px-4 py-2"
+        >
+          All Posts
+        </Link>
+        <Link
+          to="/posts?cat=cybersecurity"
+          className="hover:bg-blue-50 rounded-full px-4 py-2"
+        >
+          Cybersecurity
+        </Link>
+        <Link
+          to="/posts?cat=ethical-hacking"
+          className="hover:bg-blue-50 rounded-full px-4 py-2"
+        >
+          Ethical Hacking
+        </Link>
+        <Link
+          to="/posts?cat=tech-news"
+          className="hover:bg-blue-50 rounded-full px-4 py-2"
+        >
+          Tech News
+        </Link>
+        <Link
+          to="/posts?cat=tutorials"
+          className="hover:bg-blue-50 rounded-full px-4 py-2"
+        >
+          Tutorials
+        </Link>
+        <Link
+          to="/posts?cat=malware-analysis"
+          className="hover:bg-blue-50 rounded-full px-4 py-2"
+        >
+          Malware Analysis
+        </Link>
+        <Link
+          to="/posts?cat=networking"
+          className="hover:bg-blue-50 rounded-full px-4 py-2"
+        >
+          Networking
+        </Link>
+        <Link
+          to="/posts?cat=ai-cybersecurity"
+          className="hover:bg-blue-50 rounded-full px-4 py-2"
+        >
+          AI & Cybersecurity
+        </Link>
+        <Link
+          to="/posts?cat=tools-resources"
+          className="hover:bg-blue-50 rounded-full px-4 py-2"
+        >
+          Tools & Resources
+        </Link>
+        <Link
+          to="/posts?cat=beginners-guide"
+          className="hover:bg-blue-50 rounded-full px-4 py-2"
+        >
+          Beginner's Guide
+        </Link>
+        <Link
+          to="/posts?cat=digital-privacy"
+          className="hover:bg-blue-50 rounded-full px-4 py-2"
+        >
+          Digital Privacy
+        </Link>
+      </div>
+      <span className="text-xl font-medium">|</span>
+      {/* Search */}
+      <div className="bg-gray-100 p-2 rounded-full flex items-center gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          stroke="gray"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+        >
+          <circle cx="10.5" cy="10.5" r="7.5" />
+          <line x1="16.5" y1="16.5" x2="22" y2="22" />
+        </svg>
+        <input type="text" placeholder="search blog..." className="bg-transparent" />
+      </div>
+    </div>
+  );
+};
+
+export default MainCategories;
